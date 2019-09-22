4503f5b595d461df727d1158351b3400fbdd5ed1
Carlos Vaquedano <chvb2002@hotmail.com>
2019-09-22 09:28:41 -0600

    Desplegar el id del despositivo para enviar notificacion a usuario especifico

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ApplicationRef } from '@angular/core';
+import { PushService } from '../services/push.service';
+import { OSNotificationPayload } from '@ionic-native/onesignal/ngx';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  mensajes: OSNotificationPayload[] = [];
 
-  constructor() {}
+  constructor(public pushService: PushService,
+              private applicationRef: ApplicationRef) {}
+
+  ngOnInit(): void {
+    this.pushService.pushListener.subscribe( noti => {
+      this.mensajes.unshift( noti);
+      this.applicationRef.tick();
+    });
+
+  }
+
+  async ionViewWilEnter() {
+    console.log('will enter - cargar mensajes');
+    this.mensajes = await this.pushService.getMensajes();
+  }
+
+
+
 
 }
